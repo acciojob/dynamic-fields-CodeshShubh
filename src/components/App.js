b355f0fa4593bd2e4824d1a22ea20560824d4df5
CodeshShubh <codeshshubh@gmail.com>
2025-03-05 20:14:30 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,64 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [fields, setfields] = useState([
+    {
+      id: Date.now(),
+      name: '',
+      age: '',
+    }
+  ])
+
+
+  const addFields = ()=> {
+    setfields([...fields , { id: Date.now(), name: '',  age: '' }])
+  }
+
+  const removeFields = (id)=>{
+    const filterFildes =  fields.filter((items)=> items.id !==id )
+    setfields(filterFildes)
+  }
+
+  const submitHandler = (id, e)=>{
+    e.preventDefault();
+    const {name , value} = e.target;
+
+    setfields (
+      fields.map((items)=>{
+       return items.id ===id ? {...items, [name]:value} : items
+      })
+    )
+
+  }
+
+  const formSubmit = (e)=>{
+    e.preventDefault();
+      console.log(fields)
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{width:'50%', margin: '0 auto',   textAlign:'center'}}>
+        <form onSubmit={formSubmit}>
+        <div>
+          {
+            fields.map((items)=>{
+              return (
+                <div key={items.id}>
+                <input type="text" name="name" value={items.name} onChange={(e)=>submitHandler(items.id , e)} placeholder="Name"/>
+                <input type="number" name= "age" value={items.age} placeholder="Age" onChange={(e)=>submitHandler(items.id, e)}/>
+                <button type="button" onClick={()=>removeFields(items.id)} >Remove</button>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div>
+          <button type="button" onClick={addFields}>Add More...</button>
+          <button>Submit</button>
+        </div>
+        </form>
     </div>
   )
 }
